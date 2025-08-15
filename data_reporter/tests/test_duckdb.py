640ce5d4ab05972f5733fdb3c5a6ae5d6f125804
EdwardoCallejas <e.callejas14@yahoo.com>
2025-08-14 23:02:28 -0500

    Scaffold for duckdb csv reader
    Next is to load multiple dataframes and start using sql to manipulate data

--- data_reporter/tests/test_duckdb.py
+++ data_reporter/tests/test_duckdb.py
@@ -1,8 +1,27 @@
 import pytest
-from data_reporter.reporter import DuckDB
+import logging
+from data_reporter.reporter import QueryReader
+from data_reporter.data.config import FACT_TABLE_PATH
+import pandas as pd
+
+logging.basicConfig(level=logging.INFO)
+
 
 def test_class_import():
-    quack = DuckDB()
-    assert quack._successful_import() == True
+    qr = QueryReader(FACT_TABLE_PATH)
+    result = qr._successful_conn()
+    assert result
+
+def test_csv_str():
+    qr = QueryReader(FACT_TABLE_PATH)
+    assert qr.csv == f"read_csv_auto('{FACT_TABLE_PATH}')"
+
+def test_query():
+    qr = QueryReader(FACT_TABLE_PATH)
+    exp_cols = ['order_id' , 'customer_id', 'product_id', 'quantity', 'price', 'order_date']
+    df = qr._query_csv(f"SELECT *")
+    assert isinstance(df, pd.DataFrame)
+    assert len(df) == 10
+    assert all(exp in df.columns for exp in exp_cols)
 
 
