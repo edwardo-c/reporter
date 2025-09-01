ea035c6023ae072b38c7e35f5f4c54dfe432421b
EdwardoCallejas <e.callejas14@yahoo.com>
2025-08-31 21:19:16 -0500

    Updated docstrings for latest refactors

--- tests/tests_safe_read_xlsx/tests_cfg_normalizer.py
+++ tests/tests_safe_read_xlsx/tests_cfg_normalizer.py
@@ -40,8 +40,8 @@ def test_single_str(tmp_path):
     data = pd.DataFrame({"spam": [1,2,3], "eggs": [10,20,30]})
     p = tmp_path / "t.xlsx"
     data.to_excel(p, index=False)
-    result: dict = read_xlsx(p)
-    result_data = result["data"]
+    result: list = read_xlsx(p)
+    result_data = result[0]["data"]
     assert isinstance(result_data, pd.DataFrame) # check if data is a dataframe
     pd.testing.assert_frame_equal(data, result_data)
 
