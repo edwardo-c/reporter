97aee5dbbf302acbca27c1bebabfc48af8a41f9c
EdwardoCallejas <e.callejas14@yahoo.com>
2025-09-02 15:03:59 -0500

    Refactored stack frames
    now re-orders into schema and raises errors for missing dependencies

--- plan_executor/operations/stack_frames.py
+++ plan_executor/operations/stack_frames.py
@@ -4,20 +4,47 @@ import logging
 import pandas as pd
 
 from plan_executor.registry import register_operation
-from utils.logging_config import setup_logging
 
 logger = logging.getLogger(__name__)
 
 @register_operation("stack_frames")
-def stack_frames(frames: dict[str, pd.DataFrame], 
-                 schema: list[str], strict: bool = False):
+def stack_frames(frames: list[dict[str, pd.DataFrame]], *,
+                 schema: list[str], source: bool = True):
     """Append dataframes into a single stacked dataframe"""
     
+    if not isinstance(frames, list):
+        raise TypeError(f"expected type list, received {type(frames)}")
+
+    if not isinstance(schema, list):
+        raise ValueError(f"schema required, recieved none")
+
     # log missing fields
-    missing = _missing_fields(frames=frames, schema=schema)
-    if strict and missing:
-        raise ValueError(
-            f"missing from schema: {missing}\nschema: {schema}")
+    _missing_fields(frames=frames, schema=schema)
+    
+    data_sets = []
+
+    for f in frames:
+        # expected [{"alias": "data_1", "data": pd.DataFrame},]
+        dfx, alias = f.get("data", pd.DataFrame()).copy(), f.get("alias", "no_alias_found")
+  
+        if dfx.empty:
+            logger.warning("empty data frame detected for alias: %s!", alias)
+
+        # TODO: accept explicitly handle no alias
+
+        if source:
+            cols = [c for c in schema if c != "source"] + ["source"] 
+            dfx["source"] = alias
+        else:
+            cols = [c for c in schema if c]
+
+        dfx = dfx.reindex(columns=cols, fill_value=pd.NA) 
+     
+        data_sets.append(dfx)
+
+    # output result in given schema order
+    return pd.concat(data_sets, ignore_index=True)
+
 
 def _missing_fields(frames: list[dict[str, pd.DataFrame]], 
                     schema: list[str]) -> dict[str, list]:
@@ -29,15 +56,14 @@ def _missing_fields(frames: list[dict[str, pd.DataFrame]],
                 f"expected type dict, recieved {type(frame)}")
         
         alias = frame.get("alias")
-        data = frame.get("data", pd.DataFrame({"empty":[]}))
+        data = frame.get("data", pd.DataFrame())
         
         if data.empty:
-            raise ValueError(
-                f"missing data frame from alias: {alias}")
+            logger.info("no data found in alias: %s", alias)
         
-        m = [s for s in schema if s not in data.columns]
-        if m:
-            missing.append({"alias": alias, "missing": m})
+        miss = [s for s in schema if s not in data.columns]
+        if miss:
+            missing.append({"alias": alias, "missing": miss})
 
     if missing:         
         logger.info("%s missing %s from schema", alias, missing)    
--- tests/test_stack_frames.py
+++ tests/test_stack_frames.py
@@ -2,6 +2,7 @@ import logging
 import pytest
 
 import pandas as pd
+import numpy as np
 
 from plan_executor.operations.stack_frames import (
     _missing_fields,
@@ -9,8 +10,37 @@ from plan_executor.operations.stack_frames import (
 )
 from utils.logging_config import setup_logging
 
-setup_logging(level=logging.DEBUG)
+setup_logging(level=logging.WARNING)
 logger = logging.getLogger(__name__)
+
+def test_frame_stacking():
+    """test frame stacking, raise errors for mismatch"""
+    # Arrange
+    data_1 = pd.DataFrame(
+        {"col_a": ["a"], "col_b": ["b"]}
+    )
+
+    data_2 = pd.DataFrame(
+        {"col_a": ["a"], "col_c": ["c"]}
+    )
+    
+    frames = [
+        {"alias": "data_1", "data": data_1},
+        {"alias": "data_2", "data": data_2}
+    ]
+
+    schema = ["col_a", "col_b", "col_c"]
+
+    result = stack_frames(frames, schema=schema, source=False)
+
+    logging.debug(f"{result}")
+
+    expected = pd.DataFrame({
+        "col_a": ["a", "a"], 
+        "col_b": ["b", np.nan], 
+        "col_c": [np.nan, "c"]})
+
+    pd.testing.assert_frame_equal(result, expected)
 
 def test_missing_fields():
     """Pass missing fields, expecting raised value error"""
@@ -43,29 +73,4 @@ def test_missing_fields():
     # Assert
     assert missing == expected
 
-def test_strict_mode():
-     # Arrange
-    data_1 = pd.DataFrame(
-        {"missing_a": [1, 2, 3], "missing_b": [4, 5, 6]}
-    )
-
-    data_2 = pd.DataFrame(
-        {"col_a": [1, 2, 3], "missing_b": [4, 5, 6]}
-    )
-    
-    schema = ["col_a", "col_b"]
-
-    expected = [
-        {"alias": "data_1", "missing": ["col_a", "col_b"]},
-        {"alias": "data_2", "missing": ["col_b"]}
-    ]
-
-    frames = [
-        {"alias": "data_1", "data": data_1},
-        {"alias": "data_2", "data": data_2}
-    ]
-
-    # Act
-    with pytest.raises(ValueError):
-        stack_frames(frames=frames, schema=schema, strict=True)  
     
