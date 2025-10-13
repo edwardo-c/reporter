1ae1a377634f3571b47044f2ef49c477033ee328
EdwardoCallejas <e.callejas14@yahoo.com>
2025-10-13 05:38:41 -0500

    updated status_report pipeline to use abstracted out functions
    Intention is to use all generalized functions where applicable, keeping only business logic within the pipeline module itself

--- pipelines/status_reports/data_map.py
+++ pipelines/status_reports/data_map.py
@@ -85,14 +85,14 @@ def _query(conn: object):
     WITH sales AS(
       SELECT
         acct_num,
-        SUM(CASE WHEN year = 2025 AND part_category = 'MOUNT' THEN total ELSE 0 END) AS current_year_mounts,
-        SUM(CASE WHEN year = 2025 AND part_category = 'DVLED' THEN total ELSE 0 END) AS current_year_dvled,
-        SUM(CASE WHEN year = 2025 AND part_category = 'TV' THEN total ELSE 0 END) as current_year_tech,
-        SUM(CASE WHEN year = 2025 AND part_category = 'KIOSK' THEN total ELSE 0 END) AS current_year_kiosks,
-        SUM(CASE WHEN year = 2024 AND part_category = 'MOUNT' THEN total ELSE 0 END) as previous_year_mounts,
-        SUM(CASE WHEN year = 2024 AND part_category = 'DVLED' THEN total ELSE 0 END) as previous_year_dvled,
-        SUM(CASE WHEN year = 2024 AND part_category = 'KIOSK' THEN total ELSE 0 END) as previous_year_kiosks,
-        SUM(CASE WHEN year = 2024 AND part_category = 'TV' THEN total ELSE 0 END) AS previous_year_tech
+        SUM(CASE WHEN year = 2025 AND part_category = 'MOUNT' THEN amount ELSE 0 END) AS current_year_mounts,
+        SUM(CASE WHEN year = 2025 AND part_category = 'DVLED' THEN amount ELSE 0 END) AS current_year_dvled,
+        SUM(CASE WHEN year = 2025 AND part_category = 'TV' THEN amount ELSE 0 END) as current_year_tech,
+        SUM(CASE WHEN year = 2025 AND part_category = 'KIOSK' THEN amount ELSE 0 END) AS current_year_kiosks,
+        SUM(CASE WHEN year = 2024 AND part_category = 'MOUNT' THEN amount ELSE 0 END) as previous_year_mounts,
+        SUM(CASE WHEN year = 2024 AND part_category = 'DVLED' THEN amount ELSE 0 END) as previous_year_dvled,
+        SUM(CASE WHEN year = 2024 AND part_category = 'KIOSK' THEN amount ELSE 0 END) as previous_year_kiosks,
+        SUM(CASE WHEN year = 2024 AND part_category = 'TV' THEN amount ELSE 0 END) AS previous_year_tech
       FROM category_sales
       GROUP BY acct_num
     )
--- pipelines/status_reports/sales_refresh.py
+++ pipelines/status_reports/sales_refresh.py
@@ -1,23 +1,17 @@
 """Refresh of direct sales in duckdb"""
 
-# Standard library imports
-from contextlib import contextmanager
-from pathlib import Path
-import shutil
-import tempfile
-
 # Third Party Imports
 import pandas as pd
+
+from data_toolkit.readers.single_file import read_data
+from data_toolkit.duckdb.duck_query import create_or_replace
 
 def refresh_data(*, data_cfg: dict, conn: object):
     """Read, clean, and load sales data from network file to duckdb"""
 
-    raw_sales = read_raw_sales_data(cfg=data_cfg)
+    raw_sales = read_data(cfg=data_cfg)
     cleaned = _clean_data(raw_sales)
-    _duckdb_load(cleaned, conn=conn)
-
-def _duckdb_load(df: pd.DataFrame, *, conn: object):
-    conn.execute(f"CREATE OR REPLACE TABLE category_sales AS SELECT * FROM df")
+    create_or_replace(table_name="category_sales", df=cleaned, conn=conn)
 
 def _clean_data(df: pd.DataFrame) -> pd.DataFrame:
     """consolidate and normalize data"""
@@ -30,42 +24,6 @@ def _clean_data(df: pd.DataFrame) -> pd.DataFrame:
         )
         # drop rows with null part_category
         .dropna(subset=["part_category"])
-        # group + aggregate
-        .groupby(["part_category", "acct_num", "year"], sort=False, as_index=False)
-        .agg(total=("amount", "sum"))
     )
     return out
-
-def read_raw_sales_data(cfg: dict):
-    """Iterate over params in cfg, read from same file"""    
-    frames = []
-    with _local_copy(Path(cfg.get("file_path"))) as safe_path:
-        for p in cfg.get("params"):
-            df = pd.read_excel(
-                    io=str(safe_path), 
-                    sheet_name=p["sheet_name"],
-                    header=p["header"],
-                    usecols=p["usecols"]
-            )
-            renamed = _column_renamer(df, p["rename_map"])
-            frames.append(renamed)
-    
-    return pd.concat(frames)
-
-def _column_renamer(df: pd.DataFrame, rename_map: dict):
-    """return dataframe with renamed column"""
-    copy = df.copy()
-    copy = copy.rename(columns=rename_map)
-    return copy
-
-@contextmanager
-def _local_copy(file_path: Path):
-    """set up and tear down of temporary directory with local copy"""
-    try:
-        temp_dir = Path(tempfile.mkdtemp())
-        dst = temp_dir / file_path.name
-        shutil.copy2(file_path, dst)
-        yield dst
-    finally:
-        shutil.rmtree(temp_dir)
     
