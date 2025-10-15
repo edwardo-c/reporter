051aabfb6b1b208b57d3c54b09d45a833ff86b8d
EdwardoCallejas <e.callejas14@yahoo.com>
2025-10-15 14:14:07 -0500

    Integration of Odata into csv partition pipeline

---
+++ pipelines/pricelist/CHANGELOG.md
@@ -0,0 +1,7 @@
+[RELEASED]
+...
+
+[UNRELEASED]
+Odata capture of price lookup all (include parameter for effective date)
+column rename cfg.yaml
+column rename function
--- pipelines/pricelist/split_into_csvs/main.py
+++ pipelines/pricelist/split_into_csvs/main.py
@@ -1,31 +1,67 @@
 # standard library imports
 from dotenv import load_dotenv
-import os
 from pathlib import Path
 
 # Third party imports
 import pandas as pd
 
 # Internal Imports
-from readers.xlReader import read_safely
-from pipelines.pricelist.split_into_csvs.csv_partitioner import run
-from config.paths import PRICE_LIST_ENV
+from config.paths import PRICE_LIST_ENV, PRICE_LIST_CFG_YAML
+from utils.acumatica_odata import get_acumatica_table
+from utils.yaml_loader import load_yaml
 
 def main():
 
     print(f"Splitting CSVs")
 
     load_dotenv(PRICE_LIST_ENV)
+    cfg: dict = load_yaml(PRICE_LIST_CFG_YAML)
 
-    out_map = {
-        "Neptune": os.getenv("NEP_OUT"), 
-        "Peerless-AV": os.getenv("PAV_OUT")}
+    odata_cfg = cfg.get("odata_cfg", {}) 
 
-    frames = read_safely(os.getenv("SRC"), stack=True)
+    df: pd.DataFrame = get_acumatica_table(
+        url=odata_cfg["url"], 
+        username=odata_cfg["username"], 
+        password=odata_cfg["password"], 
+        params=odata_cfg["params"]
+    )
     
-    run(frames, out_map=out_map)
+    # enforce column order
+    df = df[odata_cfg["column_order"]]
+
+    branded = _add_brand_column(df=df)
+
+    # hot loop
+    _export_partitioned_csv(branded, out_map=cfg["out_map"])
 
     print(f"Process Complete")
+
+def _add_brand_column(df: pd.DataFrame):
+    
+    df_copy = df.copy()
+    df_copy["brand"] = (df_copy["PriceGroup"] == "NEPTUNE")
+    df_copy["brand"] = df_copy["brand"].replace({False: "Peerless-AV", True: "Neptune"})
+
+    return df_copy
+
+def _export_partitioned_csv(
+        df: pd.DataFrame,
+        out_map: dict[str, str]
+    ) -> None | list:
+
+    """
+    partition by and export to specified folder
+    """
+    for brand, partition in df.groupby(by="brand", dropna=True):
+        
+        root_dir = Path(out_map.get(brand))
+
+        for cust, part in partition.groupby(by="Customer"):
+            
+            export_path = root_dir / f"{cust}.csv"
+            
+            part.to_csv(export_path, index=False)
+
 
 if __name__ == "__main__":
     raise SystemExit(main())
