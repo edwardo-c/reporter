50422ac910ec80b20630966b6e923894cb7d5893
EdwardoCallejas <e.callejas14@yahoo.com>
2026-02-05 13:47:55 -0600

    validation of run sql cfg complete

---
+++ data_toolkit/duckdb/execute_sql.py
@@ -0,0 +1,88 @@
+from dataclasses import dataclass, field
+
+import duckdb
+from pathlib import Path
+
+from typing import Any, Sequence, Mapping
+
+@dataclass(frozen=True)
+class SqlCfg():
+
+    base_dir: Path
+    files: tuple[Path, ...]
+
+    @staticmethod
+    def _validate_base_dir(base_dir: str | Path) -> Path:
+        if isinstance(base_dir, str):
+            base_dir = Path(base_dir)
+        if not base_dir.is_dir(): 
+            raise NotADirectoryError(f"base_dir is not a directory, got: {base_dir}")
+
+        return base_dir
+
+    @staticmethod
+    def _validate_files(
+        base_dir: Path,
+        files: str | Path | Sequence[str | Path]
+    ) -> tuple[Path, ...]:
+        
+        if isinstance(files, (str, Path)):
+            files = [files]
+        elif not isinstance(files, Sequence):
+            raise ValueError(
+                f'invalid "files" type, expected Sequence got {type(files)}'
+            )
+
+        cleaned_files = []
+
+        for f in files:
+
+            if isinstance(f, str):
+                f = Path(f)
+
+            f = base_dir / f
+
+            if not f.is_file(): raise FileNotFoundError(f"invalid file: {f}")
+            if not f.suffix.lower() == ".sql": raise ValueError(f"file must be .sql, got: {f.suffix}")
+
+            cleaned_files.append(f)
+        
+        return tuple(cleaned_files)
+
+    @classmethod
+    def from_mapping(
+        cls,
+        raw_cfg: Mapping[str, Any]
+    ):
+        
+        if not "base_dir" in raw_cfg:
+            raise KeyError(f"'base_dir' not found in raw_cfg, got {raw_cfg}")
+
+        raw_base_dir = raw_cfg["base_dir"]
+
+        clean_base_dir: Path = cls._validate_base_dir(raw_base_dir)
+
+        if not "files" in raw_cfg:
+            raise KeyError(f"'files' not found in raw_cfg, got {raw_cfg}")
+
+        raw_files = raw_cfg["files"]
+
+        cleaned_files = cls._validate_files(clean_base_dir, raw_files)
+        
+        return cls(base_dir=clean_base_dir, files=cleaned_files)
+
+def run_ordered_sql(
+        conn: duckdb.DuckDBPyConnection,
+        cfg: Mapping[str, Any],
+):
+    """
+    runs all files from config
+    """
+    sql_cfg = SqlCfg.from_mapping(cfg)
+
+    for f in sql_cfg.files:
+        sql = f.read_text(encoding="utf-8")
+        conn.execute(sql)
+
+    return sql_cfg.files
+
--- pipelines/union_all/main.py
+++ pipelines/union_all/main.py
@@ -5,54 +5,32 @@ from dotenv import load_dotenv
 import pandas as pd
 from pathlib import Path
 from data_toolkit.duckdb.register_frames import register_frames
-
-"""
-=============
-
-provide config, 
-read all, 
-process through sql pipeline, 
-fill table,
-
-==============
-
-when new data comes in?
-refresh the entire year
-
-"""
-
-"""
-can you convert this into a non-specific pipeline?
-as in: provide a config to do 'this thing' for any cfg?
-"""
+from data_toolkit.duckdb.execute_sql import run_ordered_sql
 
 
-def load_credit_events():
+def main():
 
     load_dotenv(CREDIT_EVENTS_ENV)
+    
     cfg = load_yaml(CREDIT_EVENTS_CFG)
+    
     db = cfg["database"]
+    
     sources = cfg["credit_events_pipeline"]["sources"]
-    sql = cfg["credit_events_pipeline"]["sql"]
-    sql_base_dir = Path(sql["base_dir"])
+    
+    ordered_sql = cfg["credit_events_pipeline"]["sql"]
 
     with duckdb.connect(db) as conn:
         
         register_frames(conn, sources)
        
+        run_ordered_sql(conn, ordered_sql)
+        
         breakpoint()
-        
-        # business logic
-        for step in sql["steps"]:
 
-            sql = Path(sql_base_dir / step).read_text()
-            conn.execute(sql)
-
-        # enforce CananicalSchema and apply UNION ALL
+        # enforce CanonicalSchema and apply UNION ALL
     
-        
-        
-        
+        # inspect export manually - escape hatch:
         # df = conn.execute("select * from stg_credit_events").df()
         # df.to_csv(r"C:\Users\eddiec11us\Documents\credit_events.csv", index=False)
 
@@ -65,5 +43,4 @@ with xlwings.app.CalculateState == 0
 """
 
 if __name__ == "__main__":
-
-    load_credit_events()
+    main()
